1234664e27f2a64170fa0442eba77dc6489ddd37
dkvirus <dkvirus@192.168.0.105>
2018-08-12 11:28:32 +0800

    feat:添加相册功能

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
-const app = require('express')();
+const express = require('express');
+const app = express();
+const path = require('path');
+const formidable = require('formidable');
+const fs = require('fs');
+const sd = require('silly-datetime');
 
-app.set( 'view engine','ejs' )
+app.set( 'view engine','ejs' );
+app.use(express.static(path.join(process.cwd(),'public')))
+
+app.post('/upload', function(req, res){
+    const form = new formidable.IncomingForm();
+    
+    form.uploadDir = path.join(process.cwd(), 'public/images'); //设置上传文件放在哪个目录下
+    const rand = sd.format(new Date(), 'YYYYMMDDHHmm') + Math.floor((Math.random()*10000));  //设置上传文件名产生随机数
+    
+    form.parse(req, function(err, fields,files){
+        const oldPath = files.photos.path;
+        const newPath = path.join(path.dirname(oldPath), rand + path.extname(files.photos.name));
+        fs.renameSync(oldPath, newPath);
+        res.redirect('/album');
+    })
+})
 
 app.get( '/', function( req, res ){
     res.render( 'index' )
@@ -11,7 +31,9 @@ app.get( '/blog', function(req, res){
 } )
  
 app.get( '/album', function(req, res){
-    res.render('album' )
+   const files= fs.readdirSync(path.join(process.cwd(),'public/images'))
+
+    res.render('album', {files:files})
 })
 
 app.listen( 3000, function( ){
---
+++ public/js/album.js
@@ -0,0 +1,3 @@
+function showModal () {
+    $('#myModal').modal('show');
+}
